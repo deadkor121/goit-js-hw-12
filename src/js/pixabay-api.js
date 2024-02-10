4b62fa186e2d741f220d4adda271055a90e88bbf
denis <deadkor26@gmail.com>
2024-02-10 19:40:15 +0200

    new js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,5 @@
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import closeIcon from '../img/bi_x-octagon.png';
 import axios from "axios";
-import { renderPhoto } from "./render-functions";
-import { smoothScroll } from "./render-functions";
-import { noPhotos } from "./render-functions";
-import { addLoadButton } from "./render-functions";
-import { endOfCollection } from "./render-functions";
-import { simpleLightbox } from "./render-functions";
-
-
-export const refs = {
-    form: document.querySelector('form'),
-    list: document.querySelector('.gallery'),
-    spanLoader: document.querySelector('.loader'),
-    loadButton: document.querySelector('.load-button'),
-    input: document.querySelector('.form-input')
-}
-
-let page = 1;
-let inputSearch = '';
-let perPage = 15;
-
 // ----Request function----
 export async function getPhotos() {
     refs.spanLoader.classList.remove('hidden');
@@ -39,67 +16,4 @@ export async function getPhotos() {
     const {hits, totalHits } = response.data;
     refs.spanLoader.classList.add('hidden');
     return {hits, totalHits};
-}
-
-// ----Add an event handler for the "Load more" button----
-refs.loadButton.addEventListener('click', onLoadMoreButton);
-
-// ----Add an event handler for the "Search" button----
-refs.form.addEventListener('submit', onSearchButton);
-
-// ----Event Searching photos----
-async function onSearchButton(e){
-    e.preventDefault();
-    inputSearch  = refs.input.value.trim();
-    refs.list.innerHTML = '';
-    refs.loadButton.classList.add('hidden');
-    if (inputSearch === '') {
-        return iziToast.error({
-            messageColor: '#FFF',
-            color: '#EF4040',
-            iconUrl: closeIcon,
-            position: 'topRight',
-            message: 'Please,enter what do you want to find!',
-        });
-    };
-    page = 1;
-    try {
-        const { hits, totalHits } = await getPhotos();
-        noPhotos(hits);
-        renderPhoto(hits);
-        addLoadButton(totalHits,perPage);
-    }
-    catch (error) {
-        iziToast.error({
-            messageColor: '#FFF',
-            color: '#EF4040',
-            iconUrl: closeIcon,
-            position: 'topRight',
-            message: `${error}`,
-        })
-    } 
-    simpleLightbox();
-    refs.form.reset(); 
-}
-
-// ----Event Loading photos----
- async function onLoadMoreButton() {  
-    page++;
-    try {
-        const {hits, totalHits} = await getPhotos();
-        renderPhoto(hits);
-        endOfCollection(page, totalHits, perPage);
-    }
-    catch (error) {
-        console.log(error);
-        iziToast.error({
-                messageColor: '#FFF',
-                color: '#EF4040',
-                iconUrl: closeIcon,
-                position: 'topRight',
-                message: `${error}`,
-            })
-     };
-    smoothScroll();
-    simpleLightbox();
 }
